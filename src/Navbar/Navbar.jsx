c85a28879563e671e77bc3762d8793898dc72f15
Ivaylo Yordanov <bazingaz1997@gmail.com>
2023-11-21 23:49:33 +0200

    Version u can open hamburger

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,12 +4,17 @@ import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom"
 
 function Navbar() {
 
+  function activateMenu(){
+    const menu = document.querySelector("[data-activeMenu]");
+    menu.style.display = "block";
+  }
+
   return (
     <>
         <header>
           <h1><em>Teppex</em></h1>
           <span className='hamburgerMenu'>
-            <span style={{color: 'white', fontSize: "60px", paddingRight: "20px"}} class="material-icons-sharp">menu</span>
+            <span onClick={activateMenu} style={{color: 'white', fontSize: "60px", paddingRight: "20px"}} class="material-icons-sharp">menu</span>   
           </span>
           <Link className='link' to="/">Home</Link>
           <Link className='link' to="/services">Services</Link>
@@ -17,6 +22,12 @@ function Navbar() {
           <Link className='link' to="/about">About</Link>
           <span className='red-line'></span>
         </header>
+        <div data-activeMenu className="box">
+          <Link className='link' to="/">Home</Link>
+          <Link className='link' to="/services">Services</Link>
+          <Link className='link' to="/prices">Prices</Link>
+          <Link className='link' to="/about">About</Link>
+        </div>
     </>
   )
 }
